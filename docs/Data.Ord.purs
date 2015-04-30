9efad9831a8298c9ed6c2ad1d835195a65332ab4
Harry Garrood <harry@garrood.me>
2015-04-30 18:49:57 +0100

    Add docs

---
+++ docs/Data.Ord.purs
@@ -0,0 +1,51 @@
+# Module Documentation
+
+## Module Data.Ord
+
+
+A module defining some useful types and instances surrounding the `Ord`
+type class.
+
+#### `invert`
+
+``` purescript
+invert :: Ordering -> Ordering
+```
+
+
+#### `Down`
+
+``` purescript
+newtype Down a
+  = Down a
+```
+
+A newtype wrapper which provides a reversed `Ord` instance. This allows
+you to do things like:
+
+    > sortBy (compare `on` Down) [1,2,3]
+    [3,2,1]
+
+#### `eqDown`
+
+``` purescript
+instance eqDown :: (Eq a) => Eq (Down a)
+```
+
+
+#### `showDown`
+
+``` purescript
+instance showDown :: (Show a) => Show (Down a)
+```
+
+
+#### `ordDown`
+
+``` purescript
+instance ordDown :: (Ord a) => Ord (Down a)
+```
+
+
+
+
